5465816ca87b0998b180367022458a25c6f973c8
Zack Yap <zackyap107@gmail.com>
2021-10-13 10:36:53 +0800

    add comment

--- Excersice1.php
+++ Excersice1.php
@@ -3,10 +3,13 @@
 		<body>
 			<?php
 				echo "<b>Zack Yap</b>"; 
+				//my name
 				echo "<br>";
 				echo "DDM201-007";
+				//my ID number
 				echo "<br>";
 				echo date("l jS \of F Y");
+				//today date
 			?>
 		</body>
 	</html>
