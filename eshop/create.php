a9d7fa88b6ee83757bd540bc2e23224b24736767
Zack Yap <zackyap107@gmail.com>
2021-10-30 21:44:32 +0800

    add nav

--- eshop/create.php
+++ eshop/create.php
@@ -9,10 +9,40 @@
 
 <body>
     <!-- container -->
-    <div class="container">
-        <div class="page-header">
-            <h1>Create Product</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Create Product</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
+    </nav>
+
+    <div class="container">
+
         <?php
         if ($_POST) {
             // include database connection
@@ -47,30 +77,30 @@
         }
         ?>
 
-        < !-- html form here where the product information will be entered -->
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <table class='table table-hover table-responsive table-bordered'>
-                    <tr>
-                        <td>Name</td>
-                        <td><input type='text' name='name' class='form-control' /></td>
-                    </tr>
-                    <tr>
-                        <td>Description</td>
-                        <td><textarea name='description' class='form-control'></textarea></td>
-                    </tr>
-                    <tr>
-                        <td>Price</td>
-                        <td><input type='text' name='price' class='form-control' /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <input type='submit' value='Save' class='btn btn-primary' />
-                            <a href='index.php' class='btn btn-danger'>Back to read products</a>
-                        </td>
-                    </tr>
-                </table>
-            </form>
+        <!-- html form here where the product information will be entered -->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Name</td>
+                    <td><input type='text' name='name' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Description</td>
+                    <td><textarea name='description' class='form-control'></textarea></td>
+                </tr>
+                <tr>
+                    <td>Price</td>
+                    <td><input type='text' name='price' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
     </div>
     <!-- end .container -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- eshop/customer.php
+++ eshop/customer.php
@@ -9,10 +9,40 @@
 
 <body>
     <!-- container -->
-    <div class="container">
-        <div class="page-header">
-            <h1>Register</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Register</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
+    </nav>
+
+    <div class="container">
+
         <?php
         if ($_POST) {
             // include database connection
@@ -54,44 +84,44 @@
         ?>
 
         <!-- html form here where the customers information will be entered -->
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                <table class='table table-hover table-responsive table-bordered'>
-                    <tr>
-                        <td>Username</td>
-                        <td><input type='text' name='Username' class='form-control' /></td>
-                    </tr>
-                    <tr>
-                        <td>Password</td>
-                        <td><input type='password' name='Password' class='form-control' /></td>
-                    </tr>
-                    <tr>
-                        <td>FirstName</td>
-                        <td><input type='text' name='FirstName' class='form-control' /></td>
-                    </tr>
-                    <tr>
-                        <td>LastName</td>
-                        <td><input type='text' name='LastName' class='form-control' /></td>
-                    </tr>
-                    <tr>
-                        <td>Gender</td>
-                        <td><input type='radio' id='Male' name='Gender' value='1'/>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Username</td>
+                    <td><input type='text' name='Username' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type='password' name='Password' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>FirstName</td>
+                    <td><input type='text' name='FirstName' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>LastName</td>
+                    <td><input type='text' name='LastName' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td><input type='radio' id='Male' name='Gender' value='1' />
                         <label for='Male'>Male</label>
-                        <input type='radio' id='Female' name='Gender' value='0'/>
+                        <input type='radio' id='Female' name='Gender' value='0' />
                         <label for='Female'>Female</label>
-                    </tr>
-                    <tr>
-                        <td>Date of Birth</td>
-                        <td><input type='date' name='Birthday' class='form-control' /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <input type='submit' value='Save' class='btn btn-primary' />
-                            <a href='index.php' class='btn btn-danger'>Back to read</a>
-                        </td>
-                    </tr>
-                </table>
-            </form>
+                </tr>
+                <tr>
+                    <td>Date of Birth</td>
+                    <td><input type='date' name='Birthday' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='index.php' class='btn btn-danger'>Back to read</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
     </div>
     <!-- end .container -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
