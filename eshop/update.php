371de575b7dee0ca14b88f972a455a5e8ff5c917
Zack Yap <zackyap107@gmail.com>
2021-11-30 19:37:06 +0800

    update product must fill in all info

--- eshop/update.php
+++ eshop/update.php
@@ -94,28 +94,41 @@
         // check if form was submitted
         if ($_POST) {
             try {
-                // write update query
-                // in this case, it seemed like we have so many fields to pass and
-                // it is better to label them and not use question marks
-                $query = "UPDATE products
-                SET name=:name, description=:description,
-                price=:price WHERE id = :id";
-                // prepare query for excecution
-                $stmt = $con->prepare($query);
-                // posted values
                 $name = htmlspecialchars(strip_tags($_POST['name']));
                 $description = htmlspecialchars(strip_tags($_POST['description']));
                 $price = htmlspecialchars(strip_tags($_POST['price']));
-                // bind the parameters
-                $stmt->bindParam(':name', $name);
-                $stmt->bindParam(':description', $description);
-                $stmt->bindParam(':price', $price);
-                $stmt->bindParam(':id', $id);
-                // Execute the query
-                if ($stmt->execute()) {
-                    echo "<div class='alert alert-success'>Record was updated.</div>";
-                } else {
-                    echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
+                $flag = 1;
+
+                if($name == "" ){
+                    $flag = 0;
+                    echo "<div class='alert alert-danger'>Please fill in your product name</div>";
+                }
+                if($description == "" ){
+                    $flag = 0;
+                    echo "<div class='alert alert-danger'>Please fill in your product description</div>";
+                }
+                if($price == "" ){
+                    $flag = 0;
+                    echo "<div class='alert alert-danger'>Please fill in your product price</div>";
+                }
+
+                if ($flag == 1){
+                    $query = "UPDATE products
+                    SET name=:name, description=:description,
+                    price=:price WHERE id = :id";
+                    // prepare query for excecution
+                    $stmt = $con->prepare($query);
+                    // bind the parameters
+                    $stmt->bindParam(':name', $name);
+                    $stmt->bindParam(':description', $description);
+                    $stmt->bindParam(':price', $price);
+                    $stmt->bindParam(':id', $id);
+                    // Execute the query
+                    if ($stmt->execute()) {
+                        echo "<div class='alert alert-success'>Record was updated.</div>";
+                    } else {
+                        echo "<div class='alert alert-danger'>Unable to update record. Please try again.</div>";
+                    }
                 }
             }
             // show errors
