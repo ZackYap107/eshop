5b546edabe25b0ca365f531f5dc69b30677a54db
Zack Yap <zackyap107@gmail.com>
2021-12-27 19:08:09 +0800

    nav

--- eshop/read_order.php
+++ eshop/read_order.php
@@ -17,15 +17,6 @@ include 'config/nav.php';
 <body>
 
     <!-- container -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Online Eshop</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-        </div>
-    </nav>
 
     <div class="container-fluid">
         <div class="page-header">
